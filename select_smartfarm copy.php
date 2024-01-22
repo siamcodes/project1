dbbeaee41b3468e12331c58042b03872d381eae1
siamcodes <siamcodes@gmail.com>
2024-01-22 10:32:04 +0700

    update

--- select_smartfarm copy.php
+++ select_smartfarm copy.php
@@ -7,7 +7,7 @@
   if ($result->num_rows > 0) {
   // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " -Zone: " . $row["zone"]. " -Board " . $row["board"]." -Tempolary".$row["temp"] ." - Humidity".$row["humi"]." ". $row["dmy"]. "<br>";
+        //echo "id: " . $row["id"]. " -Zone: " . $row["zone"]. " -Board " . $row["board"]." -Tempolary".$row["temp"] ." - Humidity".$row["humi"]." ". $row["dmy"]. "<br>";
     }
 
   } else {
