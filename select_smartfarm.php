93c641a4f2d230d4ecb357769203f76756a1eddf
siamcodes <siamcodes@gmail.com>
2024-01-22 11:02:23 +0700

    update

--- select_smartfarm.php
+++ select_smartfarm.php
@@ -1,36 +1,36 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"  />
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ></script>
+
 <?php
   include "connect.php";
 
   $sql = "SELECT * FROM tbl_smartfarm";
   $result = $conn->query($sql);
-
   if ($result->num_rows > 0) {
   // output data of each row
   ?>
-
-<table border="1">
- <?php
-    while($row = $result->fetch_assoc()) {
-    // echo "id: " . $row["id"]. " -Zone: " . $row["zone"]. " -Board " . $row["board"]." -Tempolary".$row["temp"] ." - Humidity".$row["humi"]." ". $row["dmy"]. "<br>";
- ?>
-    <tr>
-        <td> <?php echo $row["id"]; ?></td>
-        <td> <?php echo $row["zone"]; ?> </td>
-        <td> 3 </td>
-        <td> 4 </td>
-        <td> 5 </td>
-        <td> 6 </td>
-    </tr>
- <?php   
-    }
- ?>
-
-</table>
-
+<div class="container"> 
+    <table class="table table-hover table-striped">
+    <?php
+        while($row = $result->fetch_assoc()) {
+        // echo "id: " . $row["id"]. " -Zone: " . $row["zone"]. " -Board " . $row["board"]." -Tempolary".$row["temp"] ." - Humidity".$row["humi"]." ". $row["dmy"]. "<br>";
+    ?>
+        <tr>
+            <td> <?php echo $row["id"]; ?></td>
+            <td> <?php echo $row["zone"]; ?> </td>
+            <td> <?php echo $row["board"]; ?> </td>
+            <td> <?php echo $row["temp"]; ?> </td>
+            <td> <?php echo $row["humi"]; ?> </td>
+            <td> <?php echo $row["dmy"]; ?> </td>
+        </tr>
+    <?php   
+        }
+    ?>
+    </table>
+</div>
 <?php
   } else {
     echo "0 results";
   }
 $conn->close();
-
 ?>
